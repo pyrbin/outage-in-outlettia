272f24a25505195488de17fd7e895c667d81463c
smultronbusken <fredholmisak@outlook.com>
2021-02-06 21:39:13 +0100

    Improved Checkpoints

--- Assets/Scripts/Interaction/Checkpoint.cs
+++ Assets/Scripts/Interaction/Checkpoint.cs
@@ -6,20 +6,29 @@ using UnityEngine.Events;
 public class Checkpoint : Interactable
 {
 
+    private SpriteRenderer spriteRenderer;
+
     [SerializeField] private Transform SocketIn;
     [SerializeField] private Transform SocketOut;
     [SerializeField] private Wire wirePrefab;
+    [SerializeField] private Sprite TakenSprite;
 
     public float MaxLength;
+
+    public bool Taken { get; private set; } = false;
 
     // (OldWire, WireHolder, NewWire)
     UnityAction<Wire, WireHolder, Wire> OnChangeWire = delegate { };
 
+    void Start()
+    {
+        TryGetComponent<SpriteRenderer>(out spriteRenderer);
+    }
+
     protected override void OnInteract(Interactor user)
     {
-        if (user.TryGetComponent<WireHolder>(out WireHolder wireHolder))
+        if (user.TryGetComponent<WireHolder>(out WireHolder wireHolder) && !Taken)
         {
-
             wireHolder.transform.position = SocketIn.position;
 
             Wire oldWire = wireHolder.Wire;
@@ -33,8 +42,12 @@ public class Checkpoint : Interactable
             wireHolder.Wire = newWire;
             //wire.MaxLength = MaxLength;
 
+            spriteRenderer.sprite = TakenSprite;
+
+            Taken = true;
             OnChangeWire.Invoke(oldWire, wireHolder, newWire);
             wireHolder.NewCheckpoint(this);
+
         }
     }
 }
