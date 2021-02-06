7ad08528b9df8cdf4bc3c571904e227ddc6f2ac9
smultronbusken <fredholmisak@outlook.com>
2021-02-06 13:43:44 +0100

    Renamed PlayerAnimator.cs

--- Assets/Scripts/Animation/CharacterAnimator.cs
+++ Assets/Scripts/Animation/CharacterAnimator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class CharacterAnimator : MonoBehaviour
 {
 
     Animator animator;
