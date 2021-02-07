cbecdc7c30ad0a26b390237eb0115c7b4a7e1099
smultronbusken <fredholmisak@outlook.com>
2021-02-07 17:41:05 +0100

    Added JSAM and imported audio

---
+++ Assets/Media/Audio/AudioEnums.cs
@@ -0,0 +1,16 @@
+namespace JSAM {
+    public enum Sounds {
+        Checkpoint_Used,
+        Connect_Wire,
+        Death,
+        Jump,
+        Restart,
+        Swinging,
+        TeslaCoil,
+        UIClick,
+        Walk
+    }
+    public enum Music {
+        Ambience
+    }
+}
